398bddd74cf487f7a808044f6eac8f95c4e0940e
Unknown <jcoady@ycp.edu>
2017-01-25 20:34:08 -0600

    added functionality to buttons

--- app.py
+++ app.py
@@ -11,11 +11,18 @@ class PyEnvManagerApp(tkinter.Frame):
         self.grid()
         self.createWidgets()
 
+    def list_environments(self):
+        for env in self.env_manager.environments():
+            self.env_listbox.insert(tkinter.END, env)
+        self.env_listbox.grid(row=1, column=0)
+
     def createWidgets(self):
-        self.create_button = tkinter.Button(self, text="create")
+        self.create_button = tkinter.Button(self, text="create", command=self.create_on_click)
         self.create_button.grid(row=0, column=0)
-        self.delete_button = tkinter.Button(self, text="delete")
+        self.delete_button = tkinter.Button(self, text="delete", command=self.delete_on_click)
         self.delete_button.grid(row=0, column=1)
+        self.env_name = tkinter.Entry(self, width=20)
+        self.env_name.grid(row=0, column=2)
         self.yScroll = tkinter.Scrollbar(self, orient=tkinter.VERTICAL)
         self.yScroll.grid(row=1, column=1, sticky=tkinter.N + tkinter.S)
         self.env_listbox = tkinter.Listbox(self,
@@ -24,13 +31,15 @@ class PyEnvManagerApp(tkinter.Frame):
                                            height=10,
                                            width=50
         )
-        for env in self.env_manager.environments():
-            self.env_listbox.insert(tkinter.END, env)
-        self.env_listbox.grid(row=1, column=0)
+        self.list_environments()
 
+    def create_on_click(self):
+        self.env_manager.create_environment(self.env_name.get())
+        self.env_name.delete(0, tkinter.END)
+        self.list_environments()
 
-    # TODO: create event handler for 'create' button
-    # TODO: create event handler for 'delete' button
+    def delete_on_click(self):
+        self.env_manger.delete_environment()
 
 
 
